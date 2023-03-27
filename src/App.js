4ad475223d906955bfee53cc5b8eb409c9106162
zack8361 <zack6353@naver.com>
2023-03-27 09:20:28 +0900

    react (학원 시작)
    react (학원 시작)

--- src/App.js
+++ src/App.js
@@ -9,19 +9,13 @@ import BtnToNaver from "./components/BtnToNaver";
 // 함수형 컴포넌트
 import FunctionComponent from "./components/FunctionComponent";
 
-// userState 데이터
-import { useState } from "react";
-import colorData from "./data/color-data.json";
-
 function App() {
-  const [colors] = useState(colorData);
   return (
     <div className="App">
       <MainHeader></MainHeader>
       <ImgComponent></ImgComponent>
       <BtnToNaver></BtnToNaver>
       <FunctionComponent />
-      <ColorList color={colors} />
     </div>
   );
 }
